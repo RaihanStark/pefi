c10f3e580c6c3892373dedc1839a20c04b394df5
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2026-02-18 01:12:00 +0700

    Initialize frontend with Tailwind CSS integration and update HTML structure.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -11,69 +11,22 @@
 </script>
 
 <main>
-  <img alt="Wails logo" id="logo" src="{logo}">
-  <div class="result" id="result">{resultText}</div>
-  <div class="input-box" id="input">
-    <input autocomplete="off" bind:value={name} class="input" id="name" type="text"/>
-    <button class="btn" on:click={greet}>Greet</button>
+  <img
+    alt="Wails logo"
+    src="{logo}"
+    class="block w-1/2 h-1/2 mx-auto pt-[10%] bg-center bg-no-repeat bg-[length:100%_100%] bg-origin-content"
+  >
+  <div class="h-5 leading-5 my-6 mx-auto">{resultText}</div>
+  <div class="flex items-center justify-center">
+    <input
+      autocomplete="off"
+      bind:value={name}
+      class="border-none rounded outline-none h-[30px] leading-[30px] px-2.5 bg-[rgba(240,240,240,1)] antialiased hover:bg-white focus:bg-white"
+      type="text"
+    />
+    <button
+      class="w-[60px] h-[30px] leading-[30px] rounded border-none ml-5 px-2 cursor-pointer hover:bg-gradient-to-t hover:from-[#cfd9df] hover:to-[#e2ebf0] hover:text-[#333]"
+      on:click={greet}
+    >Greet</button>
   </div>
 </main>
-
-<style>
-
-  #logo {
-    display: block;
-    width: 50%;
-    height: 50%;
-    margin: auto;
-    padding: 10% 0 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
-  }
-
-  .result {
-    height: 20px;
-    line-height: 20px;
-    margin: 1.5rem auto;
-  }
-
-  .input-box .btn {
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 3px;
-    border: none;
-    margin: 0 0 0 20px;
-    padding: 0 8px;
-    cursor: pointer;
-  }
-
-  .input-box .btn:hover {
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
-    color: #333333;
-  }
-
-  .input-box .input {
-    border: none;
-    border-radius: 3px;
-    outline: none;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 10px;
-    background-color: rgba(240, 240, 240, 1);
-    -webkit-font-smoothing: antialiased;
-  }
-
-  .input-box .input:hover {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-  .input-box .input:focus {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-</style>
